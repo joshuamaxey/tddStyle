bb167541dc98d66e99c96b1125c1facbb1a2eaf7
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-06-13 17:48:28 -0500

    finished reverse-string (part 1)

--- aa04-tdd-style/problems/reverse-string.js
+++ aa04-tdd-style/problems/reverse-string.js
@@ -1,3 +1,18 @@
-module.exports = function reverseString(string) {
-  // Your code here 
+// module.exports =
+
+function reverseString(string) {
+  let newString = "";
+
+  for (let i = string.length - 1; i >= 0; i--) {
+
+    let char = string[i];
+
+    newString += char;
+  }
+
+  return newString;
 };
+
+console.log(reverseString("testing"));
+
+module.exports = reverseString;
--- aa04-tdd-style/test/reverse-string-spec.js
+++ aa04-tdd-style/test/reverse-string-spec.js
@@ -1,1 +1,12 @@
-// Your code here 
+const chai = require('chai').expect
+
+const { expect } = require('chai')
+const reverseString = require('../problems/reverse-string')
+
+describe('reverseString function', function () {
+    it('should return reversed string', function () {
+
+        let string = "string"
+        expect(reverseString(string)).to.equal("gnirts");
+    });
+});
